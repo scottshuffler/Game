9e2b039d8849c4b556426f087595c94c89732ea5
Scott <scottshuffler@gmail.com>
2014-07-31 13:58:04 -0400

    More kinds of all things

--- game.py
+++ game.py
@@ -1,13 +1,15 @@
 __authors__ = 'shuffleres, sinkdb'
 
+round_count = 0
+
 ''''Character'''
 health = 100000
 energy = 100
-attack = 150
+attack = 1500
 heal = 30
 
 '''Enemy'''
-enemy_health = 100000
+enemy_health = 50000
 enemy_energy = 100
 enemy_attack = 100
 enemy_heal = 40
@@ -15,21 +17,35 @@ enemy_heal = 40
 print("Your stats: ", health, energy, attack, heal)
 print("Enemy stats: ", enemy_health, enemy_energy, enemy_attack, enemy_heal)
 
-while health != 0 and enemy_health != 0:
-    userinput = raw_input("Attack or heal: ").lower()
-    if userinput == "attack":
+while health > 0 and enemy_health > 0:
+    user_input = raw_input("Attack or heal: ").lower()
+
+    '''user turn'''
+    if user_input == "attack":
         energy -= 10
         enemy_health -= attack
     else:
         energy -= 20
         health += 30
 
+    '''Enemy turn'''
     if enemy_health < 50000:
         enemy_energy -= 20
         enemy_health += 40
     else:
-        energy -= 10
+        enemy_energy -= 10
         health -= enemy_attack
 
+    '''Even rounds gain 20 energy'''
+    if round_count % 2 == 0:
+        enemy_energy += 20
+        energy += 20
+
+    '''Print stats'''
     print("Your stats: ", health, energy, attack, heal)
     print("Enemy stats: ", enemy_health, enemy_energy, enemy_attack, enemy_heal)
+
+if health >= 0:
+    print("You win")
+else:
+    print("You lose")
